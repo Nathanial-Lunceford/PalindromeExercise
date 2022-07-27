94c067948f6b0e72fe323aaa3373d03fcf89eb3d
Nate <nathanial.lunceford@gmail.com>
2022-07-27 13:49:30 -0500

    Completed Unit Tests

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -9,4 +9,26 @@ namespace PalindromeExercise
             Console.WriteLine("Hello World!");
         }
     }
+
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string s)
+        {
+            s = s.ToLower();
+
+            char[] reversedArray = s.ToCharArray();
+            Array.Reverse(reversedArray);
+
+            var reversed = new string(reversedArray);
+
+            if(s == reversed)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
 }
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,22 @@
 using System;
 using Xunit;
-
+using PalindromeExercise;
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("helicopter", false)]
+        [InlineData("skyscraper", false)]
+        [InlineData("rotator", true)]
+        public void Test1(string palindrome, bool expected)
         {
+            var test = new WordSmith();
 
+            var actual = test.IsAPalindrome(palindrome);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
